c9f430024e94c52289b8f4192c20fd9aff96c6c7
Rodrigo Souza <recsouza@gmail.com>
2020-12-17 17:30:11 -0300

    Creating locators
    Addind new locators and method to select tab buttons

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,12 +1,19 @@
 package pages;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
+import util.DriverManager;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage {
+
+    private AppiumDriver<MobileElement> driver;
+    private WebDriverWait wait;
 
     public HomePage(AppiumDriver<MobileElement> appiumDriver) {
         PageFactory.initElements(new AppiumFieldDecorator(appiumDriver), this);
@@ -14,4 +21,21 @@ public class HomePage {
 
     @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Selecione uma localidade acima\")")
     public MobileElement selecioneLocalStr;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Resultados\")")
+    public MobileElement resultadosTabBtn;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Totalização\")")
+    public MobileElement totalizacaoTabBtn;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Favoritos\")")
+    public MobileElement favoritosTabBtn;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Informações\")")
+    public MobileElement informacoesTabBtn;
+
+    public void selectTabBtn(MobileElement locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("android.view.View")));
+        locator.click();
+    }
 }
